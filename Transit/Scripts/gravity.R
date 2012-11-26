824f13db5580599a7f9bac741aa0b059f7afb98f
Greg Macfarlane <gregmacfarlane@gmail.com>
2012-11-26 13:05:39 -0500

    [Transit] Completed function to calculate random matrix of costs.

--- Transit/Scripts/gravity.R
+++ Transit/Scripts/gravity.R
@@ -88,26 +88,63 @@ costMatrix <- function(distance, transfer) {
 
 # Function for random cost matrix
 costRandomMatrix <- function(meandist = -0.5, sddist =0.5, 
-														 meantransfer = 1, sdtransfer = 0.5,
-														 timemultiplier= 2.5) {
-	# set random parameters
+														 meantransfer = 2, sdtransfer = 0.5,
+														 mintransfer = 2, maxtransfer = 15,
+														 meanmultiplier = 2.5, sdmultiplier = 1,
+														 meanspeed=20.3, sdspeed = 5.296) {
+	
+	#===== set random parameters ==========================================================
+	# transfer time
 	random.transfer <- matrix(rlnorm(81, meanlog= meantransfer, 
 																	 sdlog = sdtransfer), nrow=9, ncol=9)
-	random.distance <- rlnorm(8, meanlog= meandist, sdlog=sddist)
+		# cap and floor transfer times
+		random.transfer <- ifelse(random.transfer > maxtransfer, maxtransfer, random.transfer)
+		random.transfer <- ifelse(random.transfer < mintransfer, mintransfer, random.transfer)
+	
+	# time perception multiplier
+	time.multiplier <- rnorm(1, meanmultiplier, sdmultiplier)
+	time.multiplier <- ifelse(time.multiplier < 1.1, 1.1, time.multiplier)
+	
+	# distance between stations
+	rdist <- rlnorm(8, meanlog= meandist, sdlog=sddist)
+	
+	# speed of the system (can be uniform because distances are random)
+	randomspeed <- rnorm(1, meanspeed, sdspeed)
 	
 	
-	# Graph of distances between stations
-	d.matrix <- matrix(c(Inf,1,3,4,2,4,3,3,4,
-											 1,Inf,2,3,1,3,2,2,3,
-											 3,2,Inf,1,1,3,2,2,3,
-											 4,3,1,Inf,2,4,3,3,4,
-											 2,1,1,2,Inf,2,1,1,2,
-											 4,3,3,4,2,Inf,1,3,4,
-											 3,2,2,3,1,1,Inf,2,3,
-											 3,2,2,3,1,3,2,Inf,1,
-											 4,3,3,4,2,4,3,1,Inf),
+	#====== Graph of Distances  ==============
+	d.matrix <- matrix(c(Inf,rdist[1],sum(rdist[1:3]),sum(rdist[1:4]),sum(rdist[1:2]),
+											 	sum(rdist[c(1:2,5:6)]),sum(rdist[c(1:2,6)]),sum(rdist[c(1:2,7)]),
+											 	sum(rdist[c(1:2,7:8)]),
+											 rdist[1],Inf,sum(rdist[2:3]),sum(rdist[2:4]),rdist[2],
+											 	sum(rdist[c(2,5:6)]),sum(rdist[c(2,6)]),sum(rdist[c(2,7)]),
+											 	sum(rdist[c(2,7:8)]),
+											 sum(rdist[1:3]),sum(rdist[2:3]), Inf, rdist[4],rdist[3],
+											 	sum(rdist[c(3,5:6)]),sum(rdist[c(3,6)]),sum(rdist[c(3,7)]),
+											 	sum(rdist[c(3,7:8)]),
+											 sum(rdist[1:4]),sum(rdist[2:4]), rdist[4], Inf, sum(rdist[3:4]),
+											 	sum(rdist[c(3:4,5:6)]),sum(rdist[c(3:4,6)]),sum(rdist[c(3:4,7)]),
+											 	sum(rdist[c(3:4,7:8)]),
+											 sum(rdist[1:2]),rdist[2],rdist[3],sum(rdist[3:4]),Inf,
+											 	sum(rdist[5:6]),rdist[6],rdist[7],sum(rdist[7:8]),
+											 sum(rdist[c(1:2,5:6)]),sum(rdist[c(2,5:6)]),sum(rdist[c(3,5:6)]),
+											 	sum(rdist[c(3:4,5:6)]),sum(rdist[5:6]),Inf,rdist[5],sum(rdist[5:7]),
+											 	sum(rdist[5:8]),
+											 sum(rdist[c(1:2,6)]),sum(rdist[c(2,6)]),sum(rdist[c(3,6)]),
+											 	sum(rdist[c(3:4,6)]),sum(rdist[6]),rdist[5],Inf,sum(rdist[6:7]),
+											 	sum(rdist[6:8]),
+											 sum(rdist[c(1:2,7)]),sum(rdist[c(2,7)]),sum(rdist[c(3,7)]),
+											 	sum(rdist[c(3:4,7)]),sum(rdist[7]),sum(rdist[5:7]), sum(rdist[6:7]), 
+											 	Inf, rdist[8],
+											 sum(rdist[c(1:2,7:8)]),sum(rdist[c(2,7:8)]),sum(rdist[c(3,7:8)]),
+											 	sum(rdist[c(3:4,7:8)]),sum(rdist[7:8]),sum(rdist[5:8]), sum(rdist[6:8]),
+											 	rdist[8], Inf),
 										 nrow=9, ncol=9, TRUE)
-	# Matrix indicating whether a tranfer penalty should be applied
+	
+	traveltime <- d.matrix / randomspeed * 60 
+								# miles/(miles per hour) * minutes per hour = minutes
+	
+	# ========= Transfer Time ================================
 	t.matrix <- matrix(c(0, 0, 0, 0, 0, 1, 1, 1, 1,
 											 0, 0, 0, 0, 0, 1, 1, 1, 1,
 											 0, 0, 0, 0, 0, 1, 1, 1, 1,
@@ -118,8 +155,12 @@ costRandomMatrix <- function(meandist = -0.5, sddist =0.5,
 											 1, 1, 1, 1, 0, 0, 0, 0, 0,
 											 1, 1, 1, 1, 0, 0, 0, 0, 0),
 										 nrow=9,ncol=9, TRUE)
-	# return weighted sum of both matrices
-	return(distance*d.matrix + transfer*t.matrix)
+	
+	transfertime <- random.transfer * t.matrix
+	
+	
+	# return sum of both matrices
+	return(traveltime + timemultiplier * transfertime)
 }
 
 
